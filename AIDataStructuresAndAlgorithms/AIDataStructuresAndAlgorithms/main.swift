e97f981eccd8c934019adacf0923d40528f74d9b
aizexin <443792431@qq.com>
2021-05-21 15:00:59 +0800

    基础LinkedList

--- AIDataStructuresAndAlgorithms/AIDataStructuresAndAlgorithms/main.swift
+++ AIDataStructuresAndAlgorithms/AIDataStructuresAndAlgorithms/main.swift
@@ -7,40 +7,7 @@
 
 import Foundation
 
-public func example(of description: String, action: () -> ()) {
-    print("---Example of \(description)---")
-    action()
-    print()
-}
+//Example.performStackExample()
 
-//MARK: example Stack
-example(of: "use of a stack") {
-    var stack = Stack<Int>()
-    stack.push(1)
-    stack.push(2)
-    stack.push(3)
-    stack.push(4)
-    print(stack)
-    if let popedElement = stack.pop() {
-        assert(4 == popedElement)
-        print("poped: \(popedElement)")
-    }
-}
-example(of: "initializing a stack from an array") {
-  let array = ["A", "B", "C", "D"]
-  var stack = Stack(array)
-  print(stack)
-  stack.pop()
-}
+Example.performLinkedListExample()
 
-example(of: "initializing a stack from an array literal") {
-  var stack: Stack = [1.0, 2.0, 3.0, 4.0]
-  print(stack)
-  stack.pop()
-}
-example(of: "括号匹配") {
-    print(checkParetheses("xxx)("))
-}
-example(of: "反转数组") {
-    printInReverse(["A", "B", "C", "D"])
-}
